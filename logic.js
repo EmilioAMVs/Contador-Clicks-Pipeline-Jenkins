5658153ce2c4ac814b36d8c0b7a8cd7097909f78
EmilioAMVs <emiliocabrera321@outlook.com>
2026-01-20 00:06:46 +0000

    Deploy from CircleCI

---
+++ logic.js
@@ -0,0 +1,25 @@
+export function addAttempt(attemptsArray, clicks) {
+  attemptsArray.push(clicks);
+  return attemptsArray;
+}
+
+export function resetClicks() {
+  return 0;
+} 
+export function increment(clicks) {
+  return clicks + 1;
+}
+
+export function getBestAttempt(attempts) {
+  return attempts.length > 0 ? Math.max(...attempts) : 0;
+}
+
+// --- Compatibilidad con Node (para Jest) ---
+if (typeof module !== 'undefined') {
+  module.exports = {
+    increment,
+    resetClicks,
+    addAttempt,
+    getBestAttempt
+  };
+}
